d0ec45e8a547c15b1b50398c90e02a51c75ff037
jotavx <jotaviarruel97@gmail.com>
2024-01-12 11:00:10 -0300

    Update login.component.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/services/user.service';
@@ -12,7 +13,11 @@ export class LoginComponent implements OnInit {
   loginError = false;
   formLogin: FormGroup;
 
-  constructor(private userService: UserService, private router: Router) {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private afAuth: AngularFireAuth
+  ) {
     this.formLogin = new FormGroup({
       email: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required),
@@ -25,7 +30,10 @@ export class LoginComponent implements OnInit {
     this.userService
       .login(this.formLogin.value)
       .then((response) => {
-        console.log();
+        //Se agrega la persistencia session que al cerrar la pestaña o el navegador cierra la sesión
+        this.afAuth.setPersistence('session').then(() => {
+          console.log('Persistencia de sesión establecida correctamente');
+        });
         this.router.navigate(['/list-alumnos']);
       })
       .catch((error) => {
