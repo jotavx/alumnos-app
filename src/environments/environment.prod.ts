35715091d2394b9f8f1b2b1d82341189113f904b
jotavx <jotaviarruel97@gmail.com>
2024-02-14 12:00:48 -0300

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCm2uEQn0mLeirpQXCOJL_bxlVbCx3ryl8',
-    authDomain: 'club-picahuesos.firebaseapp.com',
-    projectId: 'club-picahuesos',
-    storageBucket: 'club-picahuesos.appspot.com',
-    messagingSenderId: '571068301942',
-    appId: '1:571068301942:web:62ff3be6aba2ba57a0d557',
+    apiKey: 'AIzaSyBRV-DyHH1K7nAbhHDQI6Yj-JjLg9dEGwI',
+    authDomain: 'alumnos-demo.firebaseapp.com',
+    projectId: 'alumnos-demo',
+    storageBucket: 'alumnos-demo.appspot.com',
+    messagingSenderId: '200798635913',
+    appId: '1:200798635913:web:5d58af9f23262641a28358',
   },
 };
